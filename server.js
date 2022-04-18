22f7e0debd03815636662f447d739e43205cadf6
Chaianant Anantachai <6306021612023@fitm.kmutnb.ac.th>
2022-04-19 03:35:12 +0700

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const { subscribe } = require('./routes/subscribers')
 
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
@@ -11,11 +12,11 @@ db.once('open',() => console.log('Connected to Database'))
 
 app.use(express.json())
 
-const subscribersRouter = require('./routes/subscribers')
-app.use('/subscribers', subscribersRouter)
+const subscriberRoute = require('./routes/subscribers')
+app.use('/subsribers', subscriberRoute)
 
 app.get("/", (req, res) => {
-    res.send("Hello Node.js REST API Server Done!");
+    res.send("Hello Node.js REST API Server DoneSDSDW!");
   });
 
 app.listen(3000, () => console.log('Server Started'))
